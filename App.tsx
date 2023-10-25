351544ac792be4a704813eb754962619a62b9396
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-10-25 17:49:10 -0300

    feat - initial project setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.eventName}>Nome do evento</Text>
+			<Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#131016",
+		padding: 24,
+	},
+	eventName: {
+		color: "#FDFCFE",
+		fontSize: 24,
+		fontWeight: "bold",
+		marginTop: 48,
+	},
+	eventDate: {
+		color: "#6B6B6B",
+		fontSize: 16,
+	},
 });
