998286d998589398c9a23e61227ef93ea65b1f57
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-10-25 18:11:16 -0300

    feat - adding input and submit button to homepage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,28 @@
 import { styles } from "./styles";
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export function Home() {
+	function handleParticipandAdd() {
+		console.log("Clicou");
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.eventName}>Nome do evento</Text>
 			<Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+			<View style={styles.form}>
+				<TextInput
+					style={styles.input}
+					placeholder="Nome do participante"
+					placeholderTextColor="#6B6B6B"
+				/>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={handleParticipandAdd}
+				>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
 		padding: 24,
 	},
 	eventName: {
-		color: "#FDFCFE",
+		color: "#FFF",
 		fontSize: 24,
 		fontWeight: "bold",
 		marginTop: 48,
@@ -15,5 +15,32 @@ export const styles = StyleSheet.create({
 	eventDate: {
 		color: "#6B6B6B",
 		fontSize: 16,
+	},
+	input: {
+		height: 56,
+		backgroundColor: "#1F1E25",
+		borderRadius: 5,
+		color: "#FFF",
+		padding: 16,
+		fontSize: 16,
+		flex: 1,
+	},
+	buttonText: {
+		color: "#FFF",
+		fontSize: 23,
+	},
+	button: {
+		width: 56,
+		height: 56,
+		borderRadius: 5,
+		backgroundColor: "#31CF67",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	form: {
+		marginTop: 36,
+		marginBottom: 42,
+		flexDirection: "row",
+		gap: 12,
 	},
 });
